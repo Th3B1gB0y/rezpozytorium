e1cf5d0cd4b8b6f8adc160a0d6d7e584bd8b4742
41B3RT <albertpiekarski79@gmail.com>
2017-09-10 20:26:43 +0200

    ddd

---
+++ Assets_2/player.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class player : MonoBehaviour {
+
+	private Rigidbody2D rb;
+	public float speed;
+	public float offset;
+
+	void Start () {
+		rb = GetComponent<Rigidbody2D> ();
+	}
+
+	void Update () {
+		speed -= offset;
+		if (speed < 0) {
+			speed = 0;
+		} else {
+			rb.AddForce (transform.right * speed);
+		}
+	}
+}
