aa261da79ad92362906e038c8018d4ababd215df
41B3RT <albertpiekarski79@gmail.com>
2017-09-24 19:47:47 +0200

    game update

---
+++ terrorist/Assets/scripts/player.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class player : MonoBehaviour {
+
+	private Rigidbody2D rb;
+	public Rigidbody2D[] _rb;
+	private Animator anim;
+	public float speed;
+
+	void Start () {
+		rb = GetComponent<Rigidbody2D> ();
+		anim = GetComponent<Animator> ();
+	}
+
+	void Update () {
+		rb.AddForce (transform.right * speed);
+	}
+	void OnTriggerEnter2D(Collider2D col){
+		if (col.tag == "tir") {
+			ded ();
+		}
+	}
+	void ded(){
+		anim.enabled = false;
+		for (int i = 0; i < 10; i++) {
+			if (_rb [i] != null) {
+				_rb [i].mass = 0.1f;
+				_rb [i].isKinematic = false;
+			} 
+		}
+	}
+}
---
+++ terrorist/Assets/scripts/spawner.cs
@@ -0,0 +1,105 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class spawner : MonoBehaviour {
+	
+	public GameObject guy;
+	public GameObject droga0;
+	public GameObject droga1;
+	public GameObject droga2;
+	public GameObject droga3;
+	public GameObject most;
+	public GameObject budynek;
+	public GameObject current;
+	public Transform spawn;
+	public Transform destroyer;
+	public Transform kierowca;
+	public Transform cam;
+	public WheelJoint2D[] kola;
+	public float time = 1.2f;
+	public float size;
+	float total=0;
+	public int a = 1000;
+	float ped;
+	public float dist = 15;
+	float totalz = 0;
+	int x;
+	int rand;
+	int randx;
+
+	void Start(){
+		GameObject first = Instantiate(droga0, new Vector2(0,0), Quaternion.identity)as GameObject;
+		total += 1;
+		current = first;
+		StartCoroutine (spawn_guys());
+	}
+	void Update(){
+		float speed = Input.GetAxisRaw ("Horizontal");
+		if (speed == 1) {
+			ped += Time.deltaTime*0.2f;
+			Debug.Log (ped);
+		} 
+		else if (speed == -1) {
+			ped -= Time.deltaTime*0.2f;
+			speed = -speed;
+		} 
+		else {
+			ped = 0;
+		}
+		if (speed == -1) {
+			ped += Time.deltaTime*0.2f;
+		} 
+		if (ped > 1) {
+			ped = 1;
+		} 
+		else if (ped < -1) {
+			ped = -1;
+		}
+		JointMotor2D motor = new JointMotor2D{ motorSpeed = speed * a*ped, maxMotorTorque = 2000 };
+		for(int i = 0;i< 5;i++) {
+			kola [i].motor = motor;
+		}
+		x = (int)(Random.value * 15);
+		rand = (int)(Random.value * 3);
+		randx = (int)(Random.value * -3);
+		spawn.position = gameObject.transform.position + new Vector3(17,0,0);
+		destroyer.position = gameObject.transform.position - new Vector3(20,10,0);
+		cam.rotation = Quaternion.identity;
+		kierowca.rotation = gameObject.transform.rotation;
+		generate ();
+	}
+	IEnumerator spawn_guys(){
+		for(int i = 0;i < Mathf.Infinity;i++) {
+			Instantiate (guy, spawn.position, spawn.rotation);
+			yield return new WaitForSeconds (1.2f);
+		}
+	}
+	void generate(){
+		if (current != null) {
+			if (transform.localPosition.x > current.transform.localPosition.x - 20) {
+				float c = current.transform.localPosition.x;
+				if (current.name == "droga 2 2(Clone)") {
+					c += 3.6f;
+				}
+				GameObject obj = null;
+				GameObject bud = null;
+				if (x >= 5) {
+					obj = Instantiate (droga0, new Vector2 (c+size-0.1f, totalz), Quaternion.identity)as GameObject;
+					bud = Instantiate (budynek, new Vector3 (c+size+randx, totalz+8 + (rand * 0.4f) , -dist*((-rand)*0.2f)), Quaternion.identity)as GameObject;
+				} else if (x == 3) {
+					obj = Instantiate (most, new Vector2 (c+size-0.1f+3.5f, totalz - 0.2f), Quaternion.identity)as GameObject;
+				} else if (x > 3 && x < 5) {
+					obj = Instantiate (droga1, new Vector2 (c+size-0.1f, totalz), Quaternion.identity)as GameObject;
+					totalz -= 2.1f;
+				} else if (x == 2){
+					obj = Instantiate (droga3, new Vector2 (c+size-0.1f, totalz+2.1f), Quaternion.identity)as GameObject;
+					totalz += 2.1f;
+				} else if (x <= 1) {
+					obj = Instantiate (droga2, new Vector2 (c+size-0.1f, totalz), Quaternion.identity)as GameObject;
+				}
+				current = obj;
+				bud.GetComponent<Renderer>().sortingOrder = -rand-3;
+			}
+		}
+	}
+}
