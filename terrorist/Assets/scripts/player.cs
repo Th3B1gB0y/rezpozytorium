3080771dc0cfa4fdb89dca9cd5954ce1330d315f
41B3RT <albertpiekarski79@gmail.com>
2017-10-09 22:21:10 +0200

    game update 091017
    nope

--- terrorist/Assets/scripts/player.cs
+++ terrorist/Assets/scripts/player.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class player : MonoBehaviour {
 
@@ -7,6 +8,8 @@ public class player : MonoBehaviour {
 	public Rigidbody2D[] _rb;
 	private Animator anim;
 	public float speed;
+	public static bool dead = false;
+	List<GameObject> obj = new List<GameObject>();
 
 	void Start () {
 		rb = GetComponent<Rigidbody2D> ();
@@ -15,10 +18,35 @@ public class player : MonoBehaviour {
 
 	void Update () {
 		rb.AddForce (transform.right * speed);
+		rb.AddForce (transform.up * 0.5f);
+		foreach (GameObject ob in obj) {
+			if (ob == null) {
+				obj.Remove(ob);
+			}
+		}
+		RaycastHit2D hit = Physics2D.Raycast(transform.position, -Vector2.up);
+		if (hit.collider != null) {
+			if (hit.distance < 0.2f) {
+				rb.AddForce (transform.up * 2);
+			}
+			if (hit.distance > 0.4f) {
+				rb.AddForce (-transform.up * 10);
+			}
+		}
 	}
 	void OnTriggerEnter2D(Collider2D col){
-		if (col.tag == "tir") {
-			ded ();
+			if (col.tag == "tir") {
+			int i = 0;
+			foreach (GameObject ob in obj) {
+				if (ob == gameObject) {
+					i += 1;
+				}
+			}
+			if (i == 0) {
+				obj.Add (gameObject);
+				spawner.kills += 1;
+				ded ();
+			}
 		}
 	}
 	void ded(){
--- terrorist/Assets/scripts/spawner.cs
+++ terrorist/Assets/scripts/spawner.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class spawner : MonoBehaviour {
 	
@@ -11,13 +12,20 @@ public class spawner : MonoBehaviour {
 	public GameObject most;
 	public GameObject budynek;
 	public GameObject current;
+	public GameObject menu;
 	public Transform spawn;
 	public Transform destroyer;
 	public Transform kierowca;
 	public Transform cam;
 	public WheelJoint2D[] kola;
+	public Text killstxt;
+	public static int kills = 0;
 	public float time = 1.2f;
 	public float size;
+	public Animator menu_anim;
+	float origin_rozped = 1000;
+	int rozped = 1000;
+	bool over = false;
 	float total=0;
 	public int a = 1000;
 	float ped;
@@ -26,46 +34,64 @@ public class spawner : MonoBehaviour {
 	int x;
 	int rand;
 	int randx;
+	bool teren = false;
 
 	void Start(){
 		GameObject first = Instantiate(droga0, new Vector2(0,0), Quaternion.identity)as GameObject;
 		total += 1;
 		current = first;
+		menu.SetActive (false);
 		StartCoroutine (spawn_guys());
 	}
 	void Update(){
-		float speed = Input.GetAxisRaw ("Horizontal");
-		if (speed == 1) {
-			ped += Time.deltaTime*0.2f;
-			Debug.Log (ped);
-		} 
-		else if (speed == -1) {
-			ped -= Time.deltaTime*0.2f;
-			speed = -speed;
-		} 
-		else {
-			ped = 0;
+		if (!over) {
+			if (ped < 0) {
+				rozped = (int)(origin_rozped + ped * origin_rozped + 300);
+			} else {
+				rozped = (int)(origin_rozped - ped * origin_rozped + 300);
+			}
+			gameObject.GetComponent<Rigidbody2D> ().AddForce (transform.right * ped * rozped * Time.deltaTime);
+			float speed = Input.GetAxisRaw ("Horizontal");
+			killstxt.text = kills.ToString ();
+			if (speed == 1) {
+				ped += Time.deltaTime * 0.2f;
+			} else if (speed == -1) {
+				ped -= Time.deltaTime * 0.2f;
+				speed = -speed;
+			} else {
+				ped = 0;
+			}
+			if (speed == -1) {
+				ped += Time.deltaTime * 0.2f;
+			} 
+			if (ped > 1) {
+				ped = 1;
+			} else if (ped < -1) {
+				ped = -1;
+			}
+			JointMotor2D motor = new JointMotor2D{ motorSpeed = speed * a * ped, maxMotorTorque = 2000 };
+			for (int i = 0; i < 5; i++) {
+				kola [i].motor = motor;
+			}
 		}
-		if (speed == -1) {
-			ped += Time.deltaTime*0.2f;
-		} 
-		if (ped > 1) {
-			ped = 1;
-		} 
-		else if (ped < -1) {
-			ped = -1;
-		}
-		JointMotor2D motor = new JointMotor2D{ motorSpeed = speed * a*ped, maxMotorTorque = 2000 };
-		for(int i = 0;i< 5;i++) {
-			kola [i].motor = motor;
+		if (Input.GetKey (KeyCode.R)&&!over) {
+			over = true;
+			menu.SetActive (true);
+			menu_anim.SetTrigger ("over");
+			StopAllCoroutines ();
 		}
 		x = (int)(Random.value * 15);
 		rand = (int)(Random.value * 3);
 		randx = (int)(Random.value * -3);
-		spawn.position = gameObject.transform.position + new Vector3(17,0,0);
-		destroyer.position = gameObject.transform.position - new Vector3(20,10,0);
+		destroyer.position = gameObject.transform.position - new Vector3 (20, 10, 0);
 		cam.rotation = Quaternion.identity;
 		kierowca.rotation = gameObject.transform.rotation;
+
+		RaycastHit2D hit = Physics2D.Raycast(transform.position, -Vector2.up);
+		if (hit.collider != null) {
+			spawn.position = new Vector2 (gameObject.transform.localPosition.x + 17, hit.point.y-0.75f);
+		}
+
 		generate ();
 	}
 	IEnumerator spawn_guys(){
@@ -83,13 +109,18 @@ public class spawner : MonoBehaviour {
 				}
 				GameObject obj = null;
 				GameObject bud = null;
+				if (teren&&x==2) {
+					x = 5;
+					teren = false;
+				}
 				if (x >= 5) {
 					obj = Instantiate (droga0, new Vector2 (c+size-0.1f, totalz), Quaternion.identity)as GameObject;
-					bud = Instantiate (budynek, new Vector3 (c+size+randx, totalz+8 + (rand * 0.4f) , -dist*((-rand)*0.2f)), Quaternion.identity)as GameObject;
+					bud = Instantiate (budynek, new Vector3 (c+size+randx, totalz+7 + (rand * 0.4f) , -dist*((-rand)*0.9f)), Quaternion.identity)as GameObject;
 				} else if (x == 3) {
 					obj = Instantiate (most, new Vector2 (c+size-0.1f+3.5f, totalz - 0.2f), Quaternion.identity)as GameObject;
 				} else if (x > 3 && x < 5) {
 					obj = Instantiate (droga1, new Vector2 (c+size-0.1f, totalz), Quaternion.identity)as GameObject;
+					teren = true;
 					totalz -= 2.1f;
 				} else if (x == 2){
 					obj = Instantiate (droga3, new Vector2 (c+size-0.1f, totalz+2.1f), Quaternion.identity)as GameObject;
@@ -98,7 +129,9 @@ public class spawner : MonoBehaviour {
 					obj = Instantiate (droga2, new Vector2 (c+size-0.1f, totalz), Quaternion.identity)as GameObject;
 				}
 				current = obj;
-				bud.GetComponent<Renderer>().sortingOrder = -rand-3;
+				if (bud != null) {
+					bud.GetComponent<Renderer> ().sortingOrder = -rand - 3;
+				}
 			}
 		}
 	}
